4b4a287dcf5fdaf1fa5081e2603a2d608c552895
Olumayowa Taiwo <oom.taiwo@gmail.com>
2021-12-05 01:09:11 +0100

    added assets

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import React from 'react';
 import {Blog, Features, Footer, Header, Possibility, WhatGPT3} from './containers';
 import {Brand, Cta, Navbar} from './components';
 
+import './App.css';
+
 const App = () => {
     return (
         <div className="App">
-            <div className="gradient_bg">
+            <div className="gradient__bg">
                 <Navbar />
                 <Header />
             </div>
