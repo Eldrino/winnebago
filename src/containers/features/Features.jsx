344f9d519df1c00cbd3017422042e4066cfa0601
Olumayowa Taiwo <oom.taiwo@gmail.com>
2021-12-06 00:31:41 +0100

    finished case studies section

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
+import { Feature } from '../../components';
 import './features.css';
+
+const featuresData = [
+    {
+        title: 'Improving end distrusts instantly',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    },
+
+    {
+        title: 'Become the tended active',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    },
+
+    {
+        title: 'Message or am nothing',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    },
+
+    {
+        title: 'Really boy law county',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    }
+]
 
 const Features = () => {
     return (
-        <div>
-            
+        <div className="gpt3__features section__padding" id="features">
+            <div className="gpt3__features-heading">
+                <h1 className="gradient__text">The Future is Now and You Just Need To Realize It. Step into the Future Today & Make it Happen.</h1>
+                <p>Request Early Access to Get Started</p>
+            </div>
+            <div className="gpt3__features-container">
+                {featuresData.map((item, index) => (
+                    <Feature title={item.title} text={item.text} key={item.title + index} />
+                ))}
+            </div>
         </div>
     )
 }
