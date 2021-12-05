c187629a74c741a5b1c567c39d2743139f5e1eab
Olumayowa Taiwo <oom.taiwo@gmail.com>
2021-12-05 23:43:44 +0100

    finished what-is-gpt3 section

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Feature } from '../../components';
 import './whatgpt3.css';
 
 const WhatGPT3 = () => {
     return (
-        <div>
-            
+        <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+            <div className="gpt3__whatgpt3-feature">
+                <Feature title="What is GPT-3" text="Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source." />
+            </div>
+            <div className="gpt3__whatgpt3-heading">
+                <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
+                <p>Explore The Library</p>
+            </div>
+            <div className="gpt3__whatgpt3-container">
+                <Feature title="Chatbots" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " />
+                <Feature title="Knowledgebase" text="Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." />
+                <Feature title="Education" text=" Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident." />
+            </div>
         </div>
     )
 }
